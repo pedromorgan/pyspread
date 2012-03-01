5927f40aee5cbae6a92f5c8ae57219c97b704b91
Martin Manns <mmanns@gmx.net>
2012-03-01 23:21:25 +0100

    pyc files excluded from pacakging in test folders, test_gpg.py fixed

--- pyspread/src/lib/test/test_gpg.py
+++ pyspread/src/lib/test/test_gpg.py
@@ -32,32 +32,19 @@ Unit tests for gpg.py
 import wx
 app = wx.App()
 
-import os
-
 import src.lib.gpg as gpg
 from src.lib.testlib import params, pytest_generate_tests
 
-from src.config import config
-
-def test_is_pyme_present():
-    """Unit test for is_pyme_present"""
-
-    try:
-        import pyme
-        pyme_present = True
-    except ImportError:
-        pyme_present = False
-
-    assert gpg.is_pyme_present() == pyme_present
 
 def _set_sig(filename, sigfilename):
-
+    """Creates a signature sigfilename for file filename"""
 
     signature = gpg.sign(filename)
 
     sigfile = open(sigfilename, "w")
     sigfile.write(signature)
     sigfile.close()
+
 
 def test_sign():
     """Unit test for sign"""
@@ -72,15 +59,14 @@ def test_sign():
     assert valid
 
 param_verify = [ \
-    {'filename': "test1.pys", 'sigfilename': "test1.pys.sig", 'valid': True},
-    {'filename': "test1.pys", 'sigfilename': "test1.pys.empty", 'valid': False},
-    {'filename': "test1.pys", 'sigfilename': "test1.pys.nonsense",
-     'valid': False},
+    {'filename': "test1.pys", 'sigfilename': "test1.pys.sig", 'valid': 1},
+    {'filename': "test1.pys", 'sigfilename': "test1.pys.empty", 'valid': 0},
+    {'filename': "test1.pys", 'sigfilename': "test1.pys.nonsense", 'valid': 0},
 ]
+
 
 @params(param_verify)
 def test_sign_verify(filename, sigfilename, valid):
     """Unit test for verify"""
 
     assert valid == gpg.verify(sigfilename, filename)
-
