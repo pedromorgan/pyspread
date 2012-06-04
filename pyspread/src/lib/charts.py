dc89de907c5e7a21cbc5318519af5841f7b10ea3
Martin Manns <mmanns@gmx.net>
2012-06-05 00:24:59 +0200

    Line chart framework added

--- pyspread/src/lib/charts.py
+++ pyspread/src/lib/charts.py
@@ -53,3 +53,41 @@ def fig2bmp(fig, width, height):
     fig.savefig(png_stream, format='png')
 
     return wx.BitmapFromImage(wx.ImageFromStream(png_stream))
+
+
+class Chart(object):
+    """Chart base class"""
+
+    def __init__(self, x, *ys):
+        self.figure = self._get_figure(x, *ys)
+
+    def get_bmp(self, width, height):
+        """Returns wx.Bitmap of chart"""
+
+        return fig2bmp(self.figure, width, height)
+
+
+class LineChart(Chart):
+    """Line chart
+
+    Parameters
+    ----------
+    series: numpy.array
+    \tSeries that are shown in chart
+
+    """
+
+    def _get_figure(self, x, *ys):
+        """Returns figure of line chart"""
+
+        series = ((x, y, "-") for y in ys)
+
+        figure = matplotlib.pyplot.figure()
+        axis = figure.add_subplot(111)
+        args = []
+
+        for line in series:
+            args.append(*line)
+        axis.plot(args)
+
+        return figure
