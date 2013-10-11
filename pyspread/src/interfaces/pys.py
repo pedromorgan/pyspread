a6227e71cd740ae1b19da03558b54d30413d6eac
Martin Manns <mmanns@gmx.net>
2013-10-11 21:46:23 +0200

    pys2 parsers added to pys interface

--- pyspread/src/interfaces/pys.py
+++ pyspread/src/interfaces/pys.py
@@ -37,13 +37,25 @@ It is split into the following sections
 
 """
 
+import ast
 from itertools import imap
+
+from src.lib.selection import Selection
+
 
 class Pys(object):
     """Interface between data_array and pys file
 
     The pys file is read from disk with the read method.
     The pys file is written to disk with the write method.
+
+    Parameters
+    ----------
+
+    data_array: model.DataArray object
+    \tThe data_array object data structure
+    pys_file: file
+    \tFile like object in pys format
 
     """
 
@@ -68,57 +80,138 @@ class Pys(object):
 
         return tuple(imap(int, keystrings))
 
-    def read(self):
-        """Read pys file to pys file data"""
-
-    def write(self):
-        """Write pys file to disk"""
-
-    def shape2pys(self):
+    def _shape2pys(self):
         """Updates shape in pys file data"""
 
-    def pys2shape(self):
+    def _pys2shape(self):
         """Updates shape in data_array"""
 
         shape_code = self.pys_file_data["shape"]
         self.data_array.shape = self._get_key(*self._split_tidy(shape_code))
 
-    def code2pys(self):
+    def _code2pys(self):
         """Updates code in pys file data"""
 
-    def pys2code(self):
+    def _pys2code(self):
         """Updates code in pys data_array"""
 
-    def attributes2pys(self):
+        code_code = self.pys_file_data["code"]
+
+        for line in code_code.split("\n"):
+            row, col, tab, code = self._split_tidy(line, maxsplit=3)
+            key = self._get_key(row, col, tab)
+
+            self.data_array[key] = unicode(code, encoding='utf-8')
+
+    def _attributes2pys(self):
         """Updates attributes in pys file data"""
 
-    def pys2attributes(self):
+    def _pys2attributes(self):
         """Updates attributes in data_array"""
 
-    def row_heights2pys(self):
+        attributes_code = self.pys_file_data["attributes"]
+
+        for line in attributes_code.split("\n"):
+            splitline = self._split_tidy(line)
+
+            selection_data = map(ast.literal_eval, splitline[:5])
+            selection = Selection(*selection_data)
+
+            tab = int(splitline[5])
+
+            attrs = {}
+            for col, ele in enumerate(splitline[6:]):
+                if not (col % 2):
+                    # Odd entries are keys
+                    key = ast.literal_eval(ele)
+
+                else:
+                    # Even cols are values
+                    attrs[key] = ast.literal_eval(ele)
+
+            self.data_array.cell_attributes.append((selection, tab, attrs))
+
+    def _row_heights2pys(self):
         """Updates row_heights in pys file data"""
 
-    def pys2row_heights(self):
+    def _pys2row_heights(self):
         """Updates row_heights in data_array"""
 
-    def col_widths2pys(self):
+        row_heights_code = self.pys_file_data["row_heights"]
+
+        for line in row_heights_code.split("\n"):
+            # Split with maxsplit 3
+            row, tab, height = self._split_tidy(line)
+            key = self._get_key(row, tab)
+
+            try:
+                self.data_array.row_heights[key] = float(height)
+
+            except ValueError:
+                pass
+
+    def _col_widths2pys(self):
         """Updates col_widths in pys file data"""
 
-    def pys2col_widths(self):
+    def _pys2col_widths(self):
         """Updates col_widths in data_array"""
 
-    def macros2pys(self):
+        col_widths_code = self.pys_file_data["col_widths"]
+
+        for line in col_widths_code.split("\n"):
+            # Split with maxsplit 3
+            col, tab, width = self._split_tidy(line)
+            key = self._get_key(col, tab)
+
+            try:
+                self.data_array.col_widths[key] = float(width)
+
+            except ValueError:
+                pass
+
+    def _macros2pys(self):
         """Updates macros in pys file data"""
 
         self.pys_file_data["macros"] = self.data_array.dict_grid.macros
 
-    def pys2macros(self):
+    def _pys2macros(self):
         """Updates macros in data_array"""
 
         self.data_array.dict_grid.macros = self.pys_file_data["macros"]
 
-    def from_data_array(self):
-        """Updates everything in pys file data"""
+    # Access via pys file
+    # -------------------
+
+    def read(self):
+        """Read pys file to pys file data"""
+
+        raise NotImplementedError
+
+    def write(self):
+        """Write pys file to disk"""
+
+        raise NotImplementedError
+
+    def clear_file_data(self):
+        """Removes all data from file_data"""
+
+        raise NotImplementedError
+
+    # Access via model.py data
+    # ------------------------
+
+    def from_data_array(self, clear=True):
+        """Replaces everything in file_data from data_array
+
+        Parameters
+        ----------
+        clear: Bool, defaults to True
+        \tIf True file_data is repaced. If false it is updated
+
+        """
+
+        if clear:
+            self.clear_file_data()
 
         self.shape2pys()
         self.code2pys()
@@ -127,8 +220,18 @@ class Pys(object):
         self.col_widths2pys()
         self.macros2pys()
 
-    def to_data_array(self):
-        """Updates everything in data_array"""
+    def to_data_array(self, clear=True):
+        """Replaces everything in data_array from file_data
+
+        Parameters
+        ----------
+        clear: Bool, defaults to True
+        \tIf True file_data is repaced. If false it is updated
+
+        """
+
+        if clear:
+            self.clear_data_array()
 
         self.pys2shape()
         self.pys2code()
@@ -136,3 +239,8 @@ class Pys(object):
         self.pys2row_heights()
         self.pys2col_widths()
         self.pys2macros()
+
+    def clear_data_array(self):
+        """Removes all data from data_array"""
+
+        raise NotImplementedError
