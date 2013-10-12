0f965c801c19fccb47819b4656c342c2fb2a3b3a
Martin Manns <mmanns@gmx.net>
2013-10-12 11:38:36 +0200

    pys interface structure changed to line by line access

--- pyspread/src/interfaces/pys.py
+++ pyspread/src/interfaces/pys.py
@@ -38,6 +38,7 @@ It is split into the following sections
 """
 
 import ast
+from collections import OrderedDict
 from itertools import imap
 
 from src.lib.selection import Selection
@@ -63,9 +64,25 @@ class Pys(object):
         self.data_array = data_array
         self.pys_file = pys_file
 
-        # pys_file_data is a dict of the pys file section strings
-        # i.e. {"shape": "1000\t100\t3\n", ...}
-        self.pys_file_data = {}
+        self._section2reader = {
+            "Pyspread save file version": self._pys_assert_version,
+            "shape": self._pys2shape,
+            "code": self._pys2code,
+            "attributes": self._pys2attributes,
+            "row_heights": self._pys2row_heights,
+            "col_widths": self._pys2col_widths,
+            "macros": self._pys2macros,
+        }
+
+        self._section2writer = OrderedDict([
+            ("Pyspread save file version", self._version2pys),
+            ("shape", self._shape2pys),
+            ("code", self._code2pys),
+            ("attributes", self._attributes2pys),
+            ("row_heights", self._row_heights2pys),
+            ("col_widths", self._col_widths2pys),
+            ("macros", self._macros2pys),
+        ])
 
     def _split_tidy(self, string, maxsplit=None):
         """Rstrips string for \n and splits string for \t"""
@@ -80,167 +97,118 @@ class Pys(object):
 
         return tuple(imap(int, keystrings))
 
+    def _pys_assert_version(self, line):
+        """Asserts pys file version"""
+
+        assert line == "0.1"
+
+    def _version2pys(self):
+        """Writes pys fiel version"""
+
     def _shape2pys(self):
         """Updates shape in pys file data"""
 
-    def _pys2shape(self):
+    def _pys2shape(self, line):
         """Updates shape in data_array"""
 
-        shape_code = self.pys_file_data["shape"]
-        self.data_array.shape = self._get_key(*self._split_tidy(shape_code))
+        self.data_array.shape = self._get_key(*self._split_tidy(line))
 
     def _code2pys(self):
         """Updates code in pys file data"""
 
-    def _pys2code(self):
+    def _pys2code(self, line):
         """Updates code in pys data_array"""
 
-        code_code = self.pys_file_data["code"]
+        row, col, tab, code = self._split_tidy(line, maxsplit=3)
+        key = self._get_key(row, col, tab)
 
-        for line in code_code.split("\n"):
-            row, col, tab, code = self._split_tidy(line, maxsplit=3)
-            key = self._get_key(row, col, tab)
-
-            self.data_array[key] = unicode(code, encoding='utf-8')
+        self.data_array[key] = unicode(code, encoding='utf-8')
 
     def _attributes2pys(self):
         """Updates attributes in pys file data"""
 
-    def _pys2attributes(self):
+    def _pys2attributes(self, line):
         """Updates attributes in data_array"""
 
-        attributes_code = self.pys_file_data["attributes"]
+        splitline = self._split_tidy(line)
 
-        for line in attributes_code.split("\n"):
-            splitline = self._split_tidy(line)
+        selection_data = map(ast.literal_eval, splitline[:5])
+        selection = Selection(*selection_data)
 
-            selection_data = map(ast.literal_eval, splitline[:5])
-            selection = Selection(*selection_data)
+        tab = int(splitline[5])
 
-            tab = int(splitline[5])
+        attrs = {}
+        for col, ele in enumerate(splitline[6:]):
+            if not (col % 2):
+                # Odd entries are keys
+                key = ast.literal_eval(ele)
 
-            attrs = {}
-            for col, ele in enumerate(splitline[6:]):
-                if not (col % 2):
-                    # Odd entries are keys
-                    key = ast.literal_eval(ele)
+            else:
+                # Even cols are values
+                attrs[key] = ast.literal_eval(ele)
 
-                else:
-                    # Even cols are values
-                    attrs[key] = ast.literal_eval(ele)
-
-            self.data_array.cell_attributes.append((selection, tab, attrs))
+        self.data_array.cell_attributes.append((selection, tab, attrs))
 
     def _row_heights2pys(self):
         """Updates row_heights in pys file data"""
 
-    def _pys2row_heights(self):
+    def _pys2row_heights(self, line):
         """Updates row_heights in data_array"""
 
-        row_heights_code = self.pys_file_data["row_heights"]
+        # Split with maxsplit 3
+        row, tab, height = self._split_tidy(line)
+        key = self._get_key(row, tab)
 
-        for line in row_heights_code.split("\n"):
-            # Split with maxsplit 3
-            row, tab, height = self._split_tidy(line)
-            key = self._get_key(row, tab)
+        try:
+            self.data_array.row_heights[key] = float(height)
 
-            try:
-                self.data_array.row_heights[key] = float(height)
-
-            except ValueError:
-                pass
+        except ValueError:
+            pass
 
     def _col_widths2pys(self):
         """Updates col_widths in pys file data"""
 
-    def _pys2col_widths(self):
+    def _pys2col_widths(self, line):
         """Updates col_widths in data_array"""
 
-        col_widths_code = self.pys_file_data["col_widths"]
+        # Split with maxsplit 3
+        col, tab, width = self._split_tidy(line)
+        key = self._get_key(col, tab)
 
-        for line in col_widths_code.split("\n"):
-            # Split with maxsplit 3
-            col, tab, width = self._split_tidy(line)
-            key = self._get_key(col, tab)
+        try:
+            self.data_array.col_widths[key] = float(width)
 
-            try:
-                self.data_array.col_widths[key] = float(width)
-
-            except ValueError:
-                pass
+        except ValueError:
+            pass
 
     def _macros2pys(self):
         """Updates macros in pys file data"""
 
         self.pys_file_data["macros"] = self.data_array.dict_grid.macros
 
-    def _pys2macros(self):
+    def _pys2macros(self, line):
         """Updates macros in data_array"""
 
-        self.data_array.dict_grid.macros = self.pys_file_data["macros"]
-
-    # Access via pys file
-    # -------------------
-
-    def read(self):
-        """Read pys file to pys file data"""
-
-        raise NotImplementedError
-
-    def write(self):
-        """Write pys file to disk"""
-
-        raise NotImplementedError
-
-    def clear_file_data(self):
-        """Removes all data from file_data"""
-
-        raise NotImplementedError
+        self.data_array.dict_grid.macros += "\n"
+        self.data_array.dict_grid.macros += line
 
     # Access via model.py data
     # ------------------------
 
-    def from_data_array(self, clear=True):
-        """Replaces everything in file_data from data_array
+    def from_data_array(self):
+        """Replaces everything in file_data from data_array"""
 
-        Parameters
-        ----------
-        clear: Bool, defaults to True
-        \tIf True file_data is repaced. If false it is updated
+        for key in self._section2writer:
+            self._section2writer[key]()
 
-        """
+    def to_data_array(self):
+        """Replaces everything in data_array from file_data"""
 
-        if clear:
-            self.clear_file_data()
+        state = None
 
-        self.shape2pys()
-        self.code2pys()
-        self.attributes2pys()
-        self.row_heights2pys()
-        self.col_widths2pys()
-        self.macros2pys()
+        for line in self.pys_file:
+            if line in self._section2reader:
+                state = line
 
-    def to_data_array(self, clear=True):
-        """Replaces everything in data_array from file_data
-
-        Parameters
-        ----------
-        clear: Bool, defaults to True
-        \tIf True file_data is repaced. If false it is updated
-
-        """
-
-        if clear:
-            self.clear_data_array()
-
-        self.pys2shape()
-        self.pys2code()
-        self.pys2attributes()
-        self.pys2row_heights()
-        self.pys2col_widths()
-        self.pys2macros()
-
-    def clear_data_array(self):
-        """Removes all data from data_array"""
-
-        raise NotImplementedError
+            elif state is not None:
+                self._section2reader[state](line)
