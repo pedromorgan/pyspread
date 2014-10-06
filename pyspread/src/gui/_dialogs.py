77f96ec3f76ad8b612a6f19fdb7f95d66b28ba4a
Martin Manns <mmanns@gmx.net>
2014-10-06 21:59:21 +0200

    Authors updated

--- pyspread/src/gui/_dialogs.py
+++ pyspread/src/gui/_dialogs.py
@@ -1166,7 +1166,7 @@ class AboutDialog(object):
             350, wx.ClientDC(parent))
         info.WebSite = ("http://manns.github.io/pyspread/",
                         _("Pyspread Web site"))
-        info.Developers = ["Martin Manns"]
+        info.Developers = ["Martin Manns", "Jason Sexauer", "Vova Kolobok"]
         info.DocWriters = ["Martin Manns", "Bosko Markovic"]
         info.Translators = ["Joe Hansen", "Mark Haanen", "Yuri Chornoivan",
                             u"Mario Blättermann", "Christian Kirbach",
