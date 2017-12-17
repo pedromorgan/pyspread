cb956ab9add64cd634c1188850e7b9be1a7e9fae
Martin Manns <mmanns@gmx.net>
2017-12-17 15:05:41 +0100

    starter now points to system wxpython phoenix

--- pyspread.sh
+++ pyspread.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PYTHONPATH=$PYTHONPATH:/home/mn/prog/Phoenix
+#export PYTHONPATH=$PYTHONPATH:/home/mn/prog/Phoenix
 
 # Calls pyspread from top level folder of extracted tarball
 export PYTHONPATH=$PYTHONPATH:./pyspread
