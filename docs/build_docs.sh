85c8cf0c4f5cb889039488335be1e1daf9169a95
pedromorgan <pedromorgan@gmail.com>
2019-07-11 18:55:18 +0100

    inv

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -4,5 +4,5 @@ HERE_DIR=`dirname "$BASH_SOURCE"`
 ROOT_DIR=`realpath $HERE_DIR/..`
 echo $ROOT_DIR
 
-python3 $ROOT_DIR/docs/create_pyqt_objects.py pyqt5 --use-qt-uri
+#python3 $ROOT_DIR/docs/create_pyqt_objects.py pyqt5 --use-qt-uri
 sphinx-build -n -v -a -b html $ROOT_DIR/docs $ROOT_DIR/public/
